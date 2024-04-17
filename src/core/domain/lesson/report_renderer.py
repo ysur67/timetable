efdc84513a864d7704abb1015137d90341270e2b
ysur57 <rus.muratow@gmail.com>
2024-04-17 23:54:06 +0300

    add get_day_of_week

--- src/core/domain/lesson/report_renderer.py
+++ src/core/domain/lesson/report_renderer.py
@@ -5,6 +5,7 @@ from typing import Protocol, final
 from jinja2 import Environment
 
 from core.models.lessons_report import LessonsReport
+from lib.dates import get_day_of_week
 
 
 class ReportRenderer(Protocol):
@@ -19,9 +20,14 @@ class JinjaReportRenderer(ReportRenderer):
     async def render(self, report: LessonsReport) -> Sequence[str]:
         if len(report.lessons) == 0:
             template = self._env.get_template("empty_lessons_report_message.jinja2")
-            return await template.render_async(report=report)
+            return [await template.render_async(report=report)]
         template = self._env.get_template("lessons_report_message.jinja2")
         tasks = [
-            template.render_async(report=report, batch_index=batch_index) for batch_index in range(len(report.lessons))
+            template.render_async(
+                report=report,
+                batch_index=batch_index,
+                get_day_of_week=get_day_of_week,
+            )
+            for batch_index in range(len(report.lessons))
         ]
         return await asyncio.gather(*tasks)
--- src/lib/dates.py
+++ src/lib/dates.py
@@ -1,5 +1,25 @@
-from datetime import UTC, datetime
+from datetime import UTC, date, datetime
 
 
 def utc_now() -> datetime:
     return datetime.now(UTC)
+
+
+def get_day_of_week(value: date) -> str:  # noqa: PLR0911
+    match value.weekday():
+        case 0:
+            return "Понедельник"
+        case 1:
+            return "Вторник"
+        case 2:
+            return "Среда"
+        case 3:
+            return "Четверг"
+        case 4:
+            return "Пятница"
+        case 5:
+            return "Суббота"
+        case 6:
+            return "Воскресенье"
+        case _:
+            raise NotImplementedError
