50e06a0616712070c2c4bbbb0cd4485306c69194
ysur57 <rus.muratow@gmail.com>
2024-04-21 01:27:22 +0300

    remove print usage

--- src/scraping/main.py
+++ src/scraping/main.py
@@ -1,17 +1,14 @@
 import asyncio
-import time
 
 from di import create_container
 from scraping.tasks.scrape_lessons import ScrapeLessonsTask
 
 
 async def main() -> None:
-    start = time.perf_counter()
     container = create_container()
     async with container.context() as context:
         task = await context.resolve(ScrapeLessonsTask)
         await task.scrape()
-    print(f"new end: {time.perf_counter() - start}")  # noqa: T201
 
 
 if __name__ == "__main__":
